f4a460c5e6e039686a8ef3455c9359dffcc57870
giokhomaa <giokhomaa@gmail.com>
2024-06-24 22:31:25 +0400

    Serializing Relationships

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,16 @@ from rest_framework import serializers
 from storeapp.models import Product, Category
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_id', 'title', 'slug']
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'category', 'slug', 'inventory', 'old_price', 'price']
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['category_id', 'title', 'slug']
+    # category = serializers.StringRelatedField()
+    category = CategorySerializer()
