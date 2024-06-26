cccd9e495f03c817c3120060bb8868abb67cd4ba
giokhomaa <giokhomaa@gmail.com>
2024-06-27 02:01:33 +0400

    add email

--- ecommerce/yasg.py
+++ ecommerce/yasg.py
@@ -9,7 +9,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="API documentation for My Project",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@yourproject.local"),
+        contact=openapi.Contact(email="khomaainfo@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
