b53325b5718afa4565e37e1d701557c67d23447d
giokhomaa <giokhomaa@gmail.com>
2024-06-27 02:00:46 +0400

    add

--- ecommerce/yasg.py
+++ ecommerce/yasg.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="My API",
+        title="Project API",
         default_version='v1',
         description="API documentation for My Project",
         terms_of_service="https://www.google.com/policies/terms/",
